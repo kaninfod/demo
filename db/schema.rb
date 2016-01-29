ce231541918f656a69709a5ab5d1799d444ab8c2
kaninfod <martinhinge@gmail.com>
2016-01-29 21:54:59 +0100

    w

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160129171652) do
+ActiveRecord::Schema.define(version: 20160129205321) do
 
   create_table "counters", force: :cascade do |t|
     t.string   "name"
