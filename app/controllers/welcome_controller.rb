2b4093087fda071fe4803bb49ee7ce3df623f681
kaninfod <martinhinge@gmail.com>
2016-01-29 22:53:42 +0100

    sas

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,6 @@ class WelcomeController < ApplicationController
     counter = Counter.find_or_create_by(name: "default")
     counter.value = (counter.value || 0) + 1
     counter.save!
-    render text: "Counter #{counter.value}; #{Rails.version}/#{RUBY_VERSION}"
+    render text: "Counter pussy #{counter.value}; #{Rails.version}/#{RUBY_VERSION}"
   end
 end
